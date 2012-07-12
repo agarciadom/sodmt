8a6b1ebed387d853633e7c0cb99ea18016b6e832
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-07-12 22:53:50 +0200

    Move Velocity template into its own file

--- sample-composition/grinder/src/test/jython/soap.py
+++ sample-composition/grinder/src/test/jython/soap.py
@@ -5,20 +5,11 @@ from net.grinder.plugin.http    import HTTPRequest
 from org.apache.velocity.app import Velocity
 from org.apache.velocity import VelocityContext
 
-from java.io import StringWriter
+from java.io import StringWriter, FileInputStream, File
 
-## VELOCITY TEMPLATES
+## CONFIGURATION AREA
 
-TEMPLATE_LIST_ORDERS = r"""
-<soapenv:Envelope
-  xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-  xmlns:u="http://ws.sodmt.uca.es/">
-  <soapenv:Header/>
-  <soapenv:Body>
-    <u:list/>
-  </soapenv:Body>
-</soapenv:Envelope>
-"""
+TEMPLATE_DIR = "src/test/velocity"
 
 ## TEST BODY
 
@@ -29,7 +20,7 @@ class TestRunner:
         def invoke():
             response = HTTPRequest().POST(
                 "http://localhost:8080/orders",
-                render(VelocityContext(), "ListOrders", TEMPLATE_LIST_ORDERS))
+                render(VelocityContext(), "ListOrders", "listOrders.vm"))
 
             stats = grinder.statistics.getForCurrentTest()
             if stats.time > 150 or response.statusCode != 200:
@@ -42,5 +33,11 @@ class TestRunner:
 
 def render(context, logString, template):
     sWriter = StringWriter()
-    Velocity.evaluate(context, sWriter, logString, template)
-    return sWriter.toString()
+    fIS = None
+    try:
+        fIS = FileInputStream(File(TEMPLATE_DIR, template))
+        Velocity.evaluate(context, sWriter, logString, fIS)
+        return sWriter.toString()
+    finally:
+        if fIS:
+            fIS.close()
