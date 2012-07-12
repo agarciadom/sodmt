edc6865f66fc6584a716b9fd08b5b3afc24578df
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-07-12 22:46:53 +0200

    grinder: separate test invocation and report generation

--- sample-composition/grinder/src/test/jython/soap.py
+++ sample-composition/grinder/src/test/jython/soap.py
@@ -2,10 +2,14 @@ from net.grinder.script.Grinder import grinder
 from net.grinder.script         import Test
 from net.grinder.plugin.http    import HTTPRequest
 
-class TestRunner:
-    def __call__(self):
-        def invoke():
-            response = HTTPRequest().POST("http://localhost:8080/orders", """
+from org.apache.velocity.app import Velocity
+from org.apache.velocity import VelocityContext
+
+from java.io import StringWriter
+
+## VELOCITY TEMPLATES
+
+TEMPLATE_LIST_ORDERS = r"""
 <soapenv:Envelope
   xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
   xmlns:u="http://ws.sodmt.uca.es/">
@@ -14,11 +18,29 @@ class TestRunner:
     <u:list/>
   </soapenv:Body>
 </soapenv:Envelope>
-""")
+"""
+
+## TEST BODY
+
+class TestRunner:
+    def __call__(self):
+        Velocity.init()
+
+        def invoke():
+            response = HTTPRequest().POST(
+                "http://localhost:8080/orders",
+                render(VelocityContext(), "ListOrders", TEMPLATE_LIST_ORDERS))
 
             stats = grinder.statistics.getForCurrentTest()
             if stats.time > 150 or response.statusCode != 200:
                 stats.success = 0
-                     
+
         test = Test(1, "Query order by ID").wrap(invoke)
         test()
+
+## UTILITY FUNCTIONS
+
+def render(context, logString, template):
+    sWriter = StringWriter()
+    Velocity.evaluate(context, sWriter, logString, template)
+    return sWriter.toString()
