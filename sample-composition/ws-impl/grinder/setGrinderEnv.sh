a8e66b4c734634a6299147c994be82ae71d4235e
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-07-12 22:03:52 +0200

    grinder: Maven-based project for running Grinder tests

--- sample-composition/ws-impl/grinder/setGrinderEnv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export GRINDER_JAR=~/bin/grinder/lib/grinder.jar
-export GRINDER_PROPERTIES=grinder.properties
-export LC_ALL="C"
--- sample-composition/ws-impl/grinder/startConsole.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/setGrinderEnv.sh"
-java -classpath $GRINDER_JAR net.grinder.Console
