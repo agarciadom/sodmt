bda65aa93d3fa51f330a42b2168baf0eb455a5de
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-08-03 00:36:26 +0200

    grinder: done generating

--- sample-composition/grinder/src/test/jython/soap.py
+++ sample-composition/grinder/src/test/jython/soap.py
@@ -7,6 +7,7 @@ from org.apache.velocity import VelocityContext
 
 from java.io import StringWriter, FileInputStream, File
 
+from java.util import Random
 
 ## CONFIGURATION AREA
 
@@ -15,9 +16,9 @@ TEMPLATE_DIR = "src/test/velocity"
 MSGTEMPLATE_DIR = TEMPLATE_DIR + "/messages"
 
 MSGTEMPLATE_PREFIX = "\n".join((
-    "<soapenv:Envelope\n",
-    "  xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n",
-    "  <soapenv:Header/>\n",
+    "<soapenv:Envelope",
+    "  xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">",
+    "  <soapenv:Header/>",
     "  <soapenv:Body>"
 ))
 
@@ -29,9 +30,12 @@ MSGTEMPLATE_SUFFIX = "\n".join((
 
 INPUTS_DIR = TEMPLATE_DIR + "/inputs"
 
+PRNG = Random()
+
 ## TEST BODY
 
 class TestRunner:
+    nameNumberMap = {}
     lastNumber = 1
 
     def __init__(self):
@@ -40,6 +44,21 @@ class TestRunner:
         self.tests = (
             successful_before(
                 "OrdersImplService-list",
+                lambda **kw: post(url="http://localhost:8080/orders", **kw),
+                maximum = 150
+            ),
+            successful_before(
+                "OrdersImplService-query",
+                lambda **kw: post(url="http://localhost:8080/orders", **kw),
+                maximum = 150
+            ),
+            successful_before(
+                "OrdersImplService-close",
+                lambda **kw: post(url="http://localhost:8080/orders", **kw),
+                maximum = 150
+            ),
+            successful_before(
+                "OrdersImplService-evaluate",
                 lambda **kw: post(url="http://localhost:8080/orders", **kw),
                 maximum = 150
             ),
@@ -60,8 +79,13 @@ def successful_before(name, func, maximum):
             stats.success = 0
 
     inputs = build_velocity_context(File(INPUTS_DIR, name + ".vm"))
+    if name in TestRunner.nameNumberMap:
+        number = TestRunner.nameNumberMap[name]
+    else:
+        number = TestRunner.nameNumberMap[name] = TestRunner.lastNumber
+        TestRunner.lastNumber += 1
+
     test = Test(TestRunner.lastNumber, name).wrap(wrapper)
-    TestRunner.lastNumber += 1
 
     return (inputs, test)
 
@@ -77,7 +101,31 @@ def build_velocity_context(template_file):
             fInputs.close()
 
 def pick_random_input(inputsCtx):
-    return inputsCtx
+    """Picks a random set of inputs from a Velocity context. All
+    variables containing a collection are supposed to have the same
+    number of elements on their outermost dimension. This function
+    will pick a random position and select the items in every variable
+    at that position. Variables without collections in them will be
+    reused as is."""
+    random_ctx = VelocityContext()
+
+    n_tests = None
+    i_test = None
+    for key in inputsCtx.getKeys():
+        value = inputsCtx.get(key)
+        try:
+            if n_tests is None:
+                n_tests = value.size()
+                i_test = PRNG.nextInt(n_tests)
+
+            if n_tests > 0:
+                value = value.get(i_test)
+        except NoSuchMethodException:
+            pass
+
+        random_ctx.put(key, value)
+
+    return random_ctx
 
 def post(url, context, logString, template):
     return HTTPRequest().POST(
