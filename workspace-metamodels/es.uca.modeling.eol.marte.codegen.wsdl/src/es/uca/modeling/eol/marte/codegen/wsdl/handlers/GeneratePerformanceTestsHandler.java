44abe796a745884349a75b9816409aeb798357a4
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-08-06 22:23:09 +0200

    codegen.wsdl: only need to touch up the .egl templates to complete v1

--- workspace-metamodels/es.uca.modeling.eol.marte.codegen.wsdl/src/es/uca/modeling/eol/marte/codegen/wsdl/handlers/GeneratePerformanceTestsHandler.java
+++ workspace-metamodels/es.uca.modeling.eol.marte.codegen.wsdl/src/es/uca/modeling/eol/marte/codegen/wsdl/handlers/GeneratePerformanceTestsHandler.java
@@ -1,27 +1,51 @@
 package es.uca.modeling.eol.marte.codegen.wsdl.handlers;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import org.apache.xmlbeans.XmlException;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.epsilon.commons.util.StringUtil;
+import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
+import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
+import org.eclipse.epsilon.egl.util.FileUtil;
 import org.eclipse.epsilon.emc.emf.EmfModel;
-import org.eclipse.epsilon.eol.exceptions.models.EolModelElementTypeNotFoundException;
+import org.eclipse.epsilon.eol.dt.launching.EclipseContextManager;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import serviceAnalyzer.messageCatalog.ServicesDocument;
 import es.uca.modeling.eol.marte.weaving.wsdl.links.LinksPackage;
+import es.uca.modeling.eol.marte.weaving.wsdl.links.PerformanceRequirementLink;
 import es.uca.modeling.eol.marte.weaving.wsdl.links.PerformanceRequirementLinks;
+import es.uca.webservices.specgen.SpecGenerator;
+import es.uca.webservices.testgen.TestGeneratorCommand;
+import es.uca.webservices.testgen.api.generators.GenerationException;
+import es.uca.webservices.testgen.api.parsers.ParserException;
+import es.uca.webservices.testgen.parsers.MessageDirection;
 import es.uca.webservices.wsdl.analyzer.emf.catalog.MessageCatalogPackage;
+import es.uca.webservices.wsdl.analyzer.emf.catalog.TypeOperation;
+import es.uca.webservices.wsdl.analyzer.emf.catalog.TypeService;
 
 /**
  * Generates a The Grinder basic Jython script and a Velocity data file
@@ -47,18 +71,21 @@ public class GeneratePerformanceTestsHandler extends AbstractHandler {
 		return null;
 	}
 
-	private void generatePerformanceTestsFor(IFile file) throws EolModelLoadingException, IOException, EolModelElementTypeNotFoundException, CoreException {
+	private void generatePerformanceTestsFor(IFile file) throws URISyntaxException, EolRuntimeException, Exception {
 		final EmfModel weavingModel = loadWeavingModel(file);
-		final PerformanceRequirementLinks allLinks =
+		try {
+			final PerformanceRequirementLinks allLinks =
 				(PerformanceRequirementLinks)weavingModel
 					.getAllOfType("PerformanceRequirementLinks")
 					.iterator().next();
 
-		populateProject(createProject(allLinks));
+			populateProject(createProject(allLinks), weavingModel, allLinks);
+		} finally {
+			weavingModel.dispose();
+		}
 	}
 
-	private IProject createProject(final PerformanceRequirementLinks allLinks)
-			throws CoreException {
+	private IProject createProject(final PerformanceRequirementLinks allLinks) throws CoreException {
 		final String projectName = allLinks.getEclipseProjectName();
 		final IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 		final IProject project = workspaceRoot.getProject(projectName);
@@ -71,9 +98,125 @@ public class GeneratePerformanceTestsHandler extends AbstractHandler {
 		return project;
 	}
 
-	private void populateProject(IProject project) {
-		// TODO Auto-generated method stub
-		
+	private void populateProject(IProject project, EmfModel weavingModel, PerformanceRequirementLinks allLinks) throws Exception {
+		generateCode(weavingModel, project.getFile("pom.xml"));
+		generateCode(weavingModel, createTestResourceFolder(project, "jython").getFile("tests.py"));
+		generateCode(weavingModel, createTestResourceFolder(project, "config").getFile("grinder.properties"));
+
+		final Set<TypeOperation> operations = collectAllOperations(allLinks);
+		final Map<Resource, ServicesDocument> emf2XMLBeans = loadAllServiceCatalogs(operations);
+		final Map<TypeOperation, File> specFiles = generateSpecs(createTestResourceFolder(project, "spec"), operations, emf2XMLBeans);
+		generateInputs(createTestResourceFolder(project, "velocity/inputs"), allLinks, specFiles);
+		generateMessageTemplates(createTestResourceFolder(project, "velocity/messages"), operations);
+
+		project.refreshLocal(IProject.DEPTH_INFINITE, null);
+	}
+
+	private Map<TypeOperation, File> generateSpecs(IFolder folder, final Set<TypeOperation> operations, final Map<Resource, ServicesDocument> emf2XMLBeans)
+			throws ParserException, IOException
+	{
+		final Map<TypeOperation, File> specFiles = new HashMap<TypeOperation, File>();
+		final File fFolder = new File(folder.getLocationURI());
+		final SpecGenerator gen = new SpecGenerator();
+		gen.setDirection(MessageDirection.IN);
+
+		for (TypeOperation op : operations) {
+			// The operation is inside a port, which is inside a service  
+			final TypeService service = (TypeService)op.eContainer().eContainer();
+			gen.setServiceName(service.getName());
+			gen.setOperationName(op.getName());
+
+			final ServicesDocument sDoc = emf2XMLBeans.get(op.eResource());
+			final String spec = gen.generate(sDoc);
+
+			final File fSpec = new File(fFolder, service.getName() + "-" + op.getName() + ".spec");
+			FileUtil.write(fSpec, spec);
+			specFiles.put(op, fSpec);
+		}
+
+		return specFiles;
+	}
+
+	private Map<Resource, ServicesDocument> loadAllServiceCatalogs(
+			final Set<TypeOperation> operations) throws XmlException,
+			IOException {
+		final Map<Resource, ServicesDocument> emf2XMLBeans = new HashMap<Resource, ServicesDocument>();
+		for (TypeOperation op : operations) {
+			final Resource opResource = op.eResource();
+			if (!emf2XMLBeans.containsKey(opResource)) {
+				final File fResource = new File(opResource.getURI().toFileString());
+				final ServicesDocument sDoc = ServicesDocument.Factory.parse(fResource);
+				emf2XMLBeans.put(opResource, sDoc);
+			}
+		}
+		return emf2XMLBeans;
+	}
+
+	private Set<TypeOperation> collectAllOperations(PerformanceRequirementLinks allLinks) {
+		// List all selected operations
+		final Set<TypeOperation> operations = new HashSet<TypeOperation>();
+		for (PerformanceRequirementLink link : allLinks.getLinks()) {
+			operations.add(link.getOperation());
+		}
+		return operations;
+	}
+
+	private void generateInputs(IFolder folder, PerformanceRequirementLinks allLinks, Map<TypeOperation, File> specFiles) throws IOException, ParserException, GenerationException {
+		final File fFolder = new File(folder.getLocationURI());
+		for (File specFile : specFiles.values()) {
+			final File fInputs = new File(fFolder, specFile.getName().replace(".spec", ".vm"));
+			final TestGeneratorCommand cmd = new TestGeneratorCommand();
+			cmd.parseArgs(specFile.getAbsolutePath(), allLinks.getNumberInputsOnSpecChanged() + "", "--output", fInputs.getAbsolutePath());
+			cmd.run();
+		}
+	}
+
+	private void generateMessageTemplates(IFolder folder, Set<TypeOperation> operations) throws IOException {
+		final File fFolder = new File(folder.getLocationURI());
+		for (TypeOperation op : operations) {
+			final TypeService service = (TypeService)op.eContainer().eContainer();
+			final File fTemplate = new File(fFolder, service.getName() + "-" + op.getName() + ".vm");
+			final String sTemplate = op.getInput().getTemplate().getValue();
+			FileUtil.write(fTemplate, sTemplate);
+		}
+	}
+
+	private IFolder createTestResourceFolder(IProject project, String path) throws CoreException {
+		final IFolder jythonFolder = project.getFolder(new Path("src/test/" + path));
+		ensureFolderExists(jythonFolder);
+		return jythonFolder;
+	}
+
+	private void ensureFolderExists(IFolder f) throws CoreException {
+		if (f.getParent() instanceof IFolder && !f.getParent().exists()) {
+			ensureFolderExists((IFolder)f.getParent());
+		}
+		if (!f.exists()) {
+			f.create(true, true, null);
+		}
+	}
+
+	private void generateCode(EmfModel weavingModel, IFile targetIFile) throws URISyntaxException, EolRuntimeException, Exception {
+		final String pathToEGL = "/egl/" + targetIFile.getName() + ".egl";
+		final File targetFile = new File(targetIFile.getLocationURI());
+		generateCode(weavingModel, pathToEGL, targetFile);
+	}
+
+	private void generateCode(final EmfModel weavingModel, final String pathToEGL, final File targetFile)
+			throws Exception, URISyntaxException, EolRuntimeException
+	{
+		// Parse the EGL script
+		final EglFileGeneratingTemplateFactory factory = new EglFileGeneratingTemplateFactory();
+		final EglTemplateFactoryModuleAdapter eglModule = new EglTemplateFactoryModuleAdapter(factory);
+		eglModule.parse(GeneratePerformanceTestsHandler.class.getResource(pathToEGL).toURI());
+
+		// Run it
+		EclipseContextManager.setup(eglModule.getContext());
+		eglModule.getContext().getModelRepository().addModel(weavingModel);
+
+		// Save the result to a file
+		final String result = StringUtil.toString(eglModule.execute());
+		FileUtil.write(targetFile, result);
 	}
 
 	private EmfModel loadWeavingModel(final IFile file)	throws EolModelLoadingException, IOException {
