8c196e2394606115729eb17d268bf9106ee81d7e
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2012-08-15 12:27:11 +0200

    MARTE: build and test with Tycho and eunit.junit

---
+++ workspace-metamodels/es.uca.modeling.eol.marte.tests.eunit/src/es/uca/modeling/eol/marte/tests/eunit/AbstractMARTEInferenceTest.java
@@ -0,0 +1,34 @@
+package es.uca.modeling.eol.marte.tests.eunit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.epsilon.eol.models.IModel;
+import org.eclipse.epsilon.eunit.junit.IEUnitSuite;
+import org.eclipse.papyrus.MARTE.MARTEPackage;
+
+public abstract class AbstractMARTEInferenceTest implements IEUnitSuite {
+
+	@Override
+	public List<IModel> prepareModels() throws Exception {
+		final List<IModel> models = new ArrayList<IModel>();
+
+		EmfModel umlModel = new EmfModel();
+		umlModel.setName("UML");
+		umlModel.setMetamodelUri(MARTEPackage.eNS_URI);
+		umlModel.setModelFile("models/model.uml");
+		umlModel.load();
+		models.add(umlModel);
+
+		EmfModel mdModel = new EmfModel();
+		mdModel.setName("MultiDiagram");
+		mdModel.setMetamodelUri(MARTEPackage.eNS_URI);
+		mdModel.setModelFile("models/multimodel.uml");
+		mdModel.load();
+		models.add(mdModel);
+
+		return models;
+	}
+
+}
---
+++ workspace-metamodels/es.uca.modeling.eol.marte.tests.eunit/src/es/uca/modeling/eol/marte/tests/eunit/ConstraintAggregationTest.java
@@ -0,0 +1,17 @@
+package es.uca.modeling.eol.marte.tests.eunit;
+
+import java.io.File;
+import java.net.URI;
+
+import org.eclipse.epsilon.eunit.junit.EUnitTestRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(EUnitTestRunner.class)
+public class ConstraintAggregationTest extends AbstractMARTEInferenceTest {
+
+	@Override
+	public URI getModuleURI() throws Exception {
+		return new File("caggregation.eol").toURI();
+	}
+
+}
