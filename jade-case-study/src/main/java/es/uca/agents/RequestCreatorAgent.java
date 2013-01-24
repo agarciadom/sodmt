6794e5f790ef128bf7504ea8b9c8b7a13a7b926a
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-01-24 02:21:48 +0100

    JADE: add some EPL boilerplate

--- jade-case-study/src/main/java/es/uca/agents/RequestCreatorAgent.java
+++ jade-case-study/src/main/java/es/uca/agents/RequestCreatorAgent.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2013 Antonio García-Domínguez.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Antonio García-Domínguez - initial API and implementation
+ */
 package es.uca.agents;
 
 import jade.content.AgentAction;
